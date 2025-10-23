8179586e33c35407775dc8e1d3ab6b888078485d
freddy208 <kfreddypatient@gmail.com>
2025-10-23 01:42:34 +0100

    Depart d11

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/navigation";
 export default function Home() {
   const router = useRouter();
   useEffect(() => {
-    router.replace("/(public)");
+    router.replace("/");
   }, [router]);
 
   return null; // ou un loader si tu veux
